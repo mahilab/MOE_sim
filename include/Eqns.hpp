2462e61ef0d88e623d062b2aba41b285a856c6e3
nathandunk <nbd2@rice.edu>
2022-04-22 16:27:08 -0500

    initial working on updating to make it based on moe. does not build at the moment

--- include/Eqns.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <G.hpp>
-#include <M.hpp>
-#include <V.hpp>
-#include <nathanisdumb.hpp>
-#include <mass_properties.hpp>
--- src/dll_virtual.cpp
+++ src/dll_virtual.cpp
@@ -1,4 +1,5 @@
 #include "MoeModel.hpp"
+#include <Mahi/MOE.hpp>
 #include <Mahi/Com.hpp>
 #include <Mahi/Util.hpp>
 #include <Mahi/Robo.hpp>
@@ -6,24 +7,41 @@
 #include <mutex>
 #include <atomic>
 
-// This is the dll that drives the model in the Unity OpenWristSim visualization
+// This is the dll that drives the model in the MOE visualization
 // Unity intefaces via the the C methods below, mostly just to start/stop the simulation, and read joint positions
-// End-users inteface through the MelShares or at a higher level, through OpenWristSim.hpp/cpp in their own C++ code
-// This was all sort of hastily put together...it could be better!
 
 #define EXPORT extern "C" __declspec(dllexport)
-
-MoeModel g_model;
-std::thread g_thread;
-std::mutex g_mtx;
-std::atomic_bool g_stop;
 
 using namespace mahi::com;
 using namespace mahi::util;
 using namespace mahi::robo;
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+moe::MoeDynamicModel model;
+moe::MoeParams moe_params;
+std::thread thread;
+std::mutex mtx;
+std::atomic_bool stop;
+
+MatrixXd A, M;
+VectorXd b, V, G, Tau, Friction;
+
+VectorXd x, xd;
+
 void simulation()
 {
+    A        = MatrixXd::Zero(4, 4);
+    M        = MatrixXd::Zero(4, 4);
+    b        = VectorXd::Zero(4);
+    V        = VectorXd::Zero(4);
+    G        = VectorXd::Zero(4);
+    Tau      = VectorXd::Zero(4);
+    Friction = VectorXd::Zero(4);
+    x        = VectorXd::Zero(4);
+    xd       = VectorXd::Zero(4);
+
     bool started = false;
     MelShare ms_torque_0("ms_torque_0");
     MelShare ms_torque_1("ms_torque_1");
@@ -41,14 +59,14 @@ void simulation()
     Time t;
     Time sim_time = 0_ms;
     enable_realtime();
-    while (!g_stop)
+    while (!stop)
     {
         if(!started){
             if(!ms_torque_0.read_data().empty()){
                 started = true;
-                std::lock_guard<std::mutex> lock(g_mtx);
-                g_model.reset();
-                timer.restart();
+                std::lock_guard<std::mutex> lock(mtx);
+                model.reset();
+                timer.stop();
             }
         }
 
@@ -63,17 +81,31 @@ void simulation()
         tau3 = !tau3_data.empty() ? tau3_data[0] : 0.0;
 
         {
-            std::lock_guard<std::mutex> lock(g_mtx);
-            g_model.set_torques(tau0,tau1,tau2,tau3);
-            if(started) g_model.update(sim_time);
-            q0 = g_model.q0;
-            q1 = g_model.q1;
-            q2 = g_model.q2;
-            q3 = g_model.q3;
-            q0d = g_model.q0d;
-            q1d = g_model.q1d;
-            q2d = g_model.q2d;
-            q3d = g_model.q3d;
+            std::lock_guard<std::mutex> lock(mtx);
+            model.set_torques(tau0,tau1,tau2,tau3);
+            if(started){
+                Tau[0] = tau0 + hardstop_torque(moe_model,0);
+                Tau[1] = tau1 + hardstop_torque(moe_model,1);
+                Tau[2] = tau2 + hardstop_torque(moe_model,2);
+                Tau[3] = tau3 + hardstop_torque(moe_model,3);
+
+                M = model.get_M();
+                V = model.get_V();
+                G = model.get_G();
+                Friction = model.get_Friction();
+                auto A = M;
+                auto B = Tau - V*qdot - G - B - Friction;
+                x = A.inverse()*b;
+            }
+            q0 = model.q[0];
+            q1 = model.q[1];
+            q2 = model.q[2];
+            q3 = model.q[3];
+            q0d = model.qd[0];
+            q1d = model.qd[1];
+            q2d = model.qd[2];
+            q3d = model.qd[3];
+            
         }
         sim_time += 1_ms;
         ms_posvel_0.write_data({q0,q0d});
@@ -87,42 +119,41 @@ void simulation()
 
 EXPORT void stop()
 {
-    g_stop = true;
-    if (g_thread.joinable())
-        g_thread.join();
+    stop = true;
+    if (thread.joinable())
+        thread.join();
 }
 
 EXPORT void start()
 {
     stop();
-    g_model.reset();
-    g_stop = false;
-    g_thread = std::thread(simulation);
-}
-
-EXPORT void set_torques(double tau0, double tau1, double tau2, double tau3)
-{
-    std::lock_guard<std::mutex> lock(g_mtx);
-    g_model.set_torques(tau0, tau1, tau2, tau3);
-}
-
-EXPORT void set_positions(double q0, double q1, double q2, double q3)
-{
-    std::lock_guard<std::mutex> lock(g_mtx);
-    g_model.set_positions(q0, q1, q2, q3);
-}
-
-EXPORT void set_velocities(double q0d, double q1d, double q2d, double q3d)
-{
-    std::lock_guard<std::mutex> lock(g_mtx);
-    g_model.set_velocities(q0d, q1d, q2d, q3d);
+    model.update({0,0,0,0},{0,0,0,0});
+    stop = false;
+    thread = std::thread(simulation);
 }
 
 EXPORT void get_positions(double *positions)
 {
-    std::lock_guard<std::mutex> lock(g_mtx);
-    positions[0] = g_model.q0;
-    positions[1] = g_model.q1;
-    positions[2] = g_model.q2;
-    positions[3] = g_model.q3;
+    std::lock_guard<std::mutex> lock(mtx);
+    positions[0] = model.q0;
+    positions[1] = model.q1;
+    positions[2] = model.q2;
+    positions[3] = model.q3;
+}
+
+// private functions that arent available from unity
+
+// compute hardstop torque if robot is in an unavailable range
+inline double hardstop_torque(MoeDynamicModel moe_model, int joint_id) {
+    static const double hs_K = 100;
+    static const double hs_B = 0.1;
+
+    double qmin = moe_params.pos_limits_min_[joint_id];
+    double qmax = moe_params.pos_limits_max_[joint_id];
+    double q = moe_model.q[joint_id];
+    double qd = moe_model.qd[joint_id];
+
+    if      (q < qmin) return hs_K * (qmin - q) - hs_B * qd;
+    else if (q > qmax) return hs_K * (qmax - q) - hs_B * qd;
+    else               return 0;
 }
